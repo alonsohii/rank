63a0e683e2281d8c663c50bfc7eeb708a02ca51b
Alonso Hernandez <alonsohi@hotmail.com>
2017-05-22 06:44:10 -0500

    Cambios
    Cambios

--- bd/0.1/procedures/USP_UpdateMatch.sql
+++ bd/0.1/procedures/USP_UpdateMatch.sql
@@ -1,4 +1,4 @@
-drop procedure USP_UpdateMatch;
+ drop procedure USP_UpdateMatch;
 DELIMITER //
 CREATE PROCEDURE USP_UpdateMatch
 (IN idjugador int, 
@@ -11,12 +11,31 @@ BEGIN
   DECLARE conteo VARCHAR(100);
   DECLARE jugador int;
   DECLARE jugadorvisitante int;
+  DECLARE existe int;
+  DECLARE puntosLocal int;
+  DECLARE puntosVisitante int;
 case  
 
 when accion = 8 then-- insertar match
-SELECT bitgames.idvisitante , bitgames.idlocal INTO jugador , jugadorvisitante FROM bitgames WHERE idbitgames = reto;
+SELECT bitgames.idlocal , bitgames.idvisitante INTO jugador , jugadorvisitante FROM bitgames WHERE idbitgames = reto;
 IF jugador = idjugador OR jugadorvisitante = idjugador THEN
-UPDATE `bitgames` SET  bitgames.golocal = glocal , bitgames.golvisitane = gvisitante WHERE idbitgames = reto;
+UPDATE `bitgames` SET  bitgames.estatus = 2 ,  bitgames.golocal = glocal , bitgames.golvisitane = gvisitante WHERE idbitgames = reto;
+
+SELECT 1  INTO  existe  FROM bitgames WHERE bitgames.idbitgames = 1 and bitgames.estatus = 2;
+	IF existe >0 THEN
+		SELECT puntos  INTO puntosLocal FROM bp_personas  where bp_personas.idbp_personas = jugador;
+	    SELECT puntos  INTO puntosVisitante FROM bp_personas  where idbp_personas = jugadorvisitante;
+        IF glocal = gvisitante THEN
+			UPDATE bp_personas SET puntos = puntosLocal+1 WHERE idbp_personas = jugador;
+            UPDATE bp_personas SET puntos = puntosLocal+1 WHERE idbp_personas = jugadorvisitante;
+            ELSE 
+            IF glocal > gvisitante THEN
+				UPDATE bp_personas SET puntos = puntosLocal+3 WHERE idbp_personas = jugador;
+                ELSE 
+			       UPDATE bp_personas SET puntos = puntosLocal+3 WHERE idbp_personas = jugadorvisitante;
+            END IF;
+        END IF;
+	END IF;
 ELSE 
 SELECT 0 AS puedeCancelar , jugador , jugadorvisitante , idjugador;
 END IF;
